266db9801fb64d9d6b80a6e3be8f0ef0b2b09662
Mark Chen <mark@ithinkbest.com>
2015-03-31 16:36:39 +0800

    hotel_json_array_ok

--- app/src/main/java/tcnr18/com/finalproject/MainActivity.java
+++ app/src/main/java/tcnr18/com/finalproject/MainActivity.java
@@ -1,6 +1,7 @@
 package tcnr18.com.finalproject;
 
 import android.app.Activity;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -14,13 +15,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class MainActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
+    static String LOG_TAG = "MARK987";
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
      */
@@ -30,7 +47,8 @@ public class MainActivity extends ActionBarActivity
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
      */
     private CharSequence mTitle;
-    private int mSelectedItemId=0;
+    private int mSelectedItemId = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,10 +66,10 @@ public class MainActivity extends ActionBarActivity
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
-        mSelectedItemId=position;
+        mSelectedItemId = position;
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getSupportFragmentManager();
-        switch (position){
+        switch (position) {
             case 0:
 
                 fragmentManager.beginTransaction()
@@ -98,18 +116,18 @@ public class MainActivity extends ActionBarActivity
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
         actionBar.setDisplayShowTitleEnabled(true);
         //   actionBar.setTitle(mTitle);
-        switch (mSelectedItemId){
+        switch (mSelectedItemId) {
             case 0:
-                actionBar.setTitle( getString(R.string.title_section1));
+                actionBar.setTitle(getString(R.string.title_section1));
                 break;
             case 1:
-                actionBar.setTitle( getString(R.string.title_section2));
+                actionBar.setTitle(getString(R.string.title_section2));
                 break;
             case 2:
-                actionBar.setTitle( getString(R.string.title_section3));
+                actionBar.setTitle(getString(R.string.title_section3));
                 break;
             default:
-                actionBar.setTitle( getString(R.string.app_name));
+                actionBar.setTitle(getString(R.string.app_name));
                 break;
         }
 
@@ -138,11 +156,68 @@ public class MainActivity extends ActionBarActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Toast.makeText(getApplicationContext(), "... debug update JSON", Toast.LENGTH_SHORT).show();
+            // http://www.vogella.com/tutorials/AndroidJSON/article.html
+//            # Just for testing, allow network access in the main thread
+//            # NEVER use this is productive code
+            processJson();
         }
 
-        return super.onOptionsItemSelected(item);
+        return true;
     }
+
+//    return super.onOptionsItemSelected(item);
+
+//}
+    void processJson(){
+        StrictMode.ThreadPolicy policy = new StrictMode.
+                ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        String input = readBugzilla();
+        Log.d(LOG_TAG, "input="+input.substring(0,100));
+        try {
+            JSONArray jsonArray=new JSONArray(input);
+
+            for (int i=0;i<jsonArray.length();i++) {
+                JSONObject json = jsonArray.getJSONObject(i);
+                String name=json.getString("name");
+
+                Log.d(LOG_TAG, "json "+i+ " is " + name);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    public String readBugzilla() {
+        StringBuilder builder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+//        HttpGet httpGet = new HttpGet("https://bugzilla.mozilla.org/rest/bug?assigned_to=lhenry@mozilla.com");
+        String str="http://data.taipei.gov.tw/opendata/apply/json/QTdBNEQ5NkQtQkM3MS00QUI2LUJENTctODI0QTM5MkIwMUZE";
+        HttpGet httpGet = new HttpGet(str);
+        Log.d(LOG_TAG, "new HttpGet(str) => "+str);
+        try {
+            HttpResponse response = client.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) {
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+            } else {
+                Log.e(LOG_TAG, "Failed to download file");
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+
 
     /**
      * A placeholder fragment containing a simple view.
@@ -183,6 +258,7 @@ public class MainActivity extends ActionBarActivity
                     getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
+
     public static class Act0Fragment extends Fragment implements View.OnClickListener {
         /**
          * The fragment argument representing the section number for this
@@ -191,6 +267,7 @@ public class MainActivity extends ActionBarActivity
         private static final String ARG_SECTION_NUMBER = "section_number";
         ImageButton btn0;
         ImageButton btn1;
+
         /**
          * Returns a new instance of this fragment for the given section
          * number.
@@ -210,16 +287,18 @@ public class MainActivity extends ActionBarActivity
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_act0, container, false);
-            btn0 =(ImageButton)rootView.findViewById(R.id.Vendor0);
+            btn0 = (ImageButton) rootView.findViewById(R.id.Vendor0);
             btn0.setOnClickListener(this);
-            btn1 =(ImageButton)rootView.findViewById(R.id.Vendor1);
+            btn1 = (ImageButton) rootView.findViewById(R.id.Vendor1);
             btn1.setOnClickListener(this);
             return rootView;
         }
-public void onClick(View v){
-    Log.d("debug", "v="+v.toString());
 
-}
+        public void onClick(View v) {
+            Log.d("debug", "v=" + v.toString());
+
+        }
+
         @Override
         public void onAttach(Activity activity) {
             super.onAttach(activity);
@@ -227,6 +306,7 @@ public void onClick(View v){
 //                    getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
+
     public static class Act1Fragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
@@ -263,6 +343,7 @@ public void onClick(View v){
 //                    getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
+
     public static class Act2Fragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
